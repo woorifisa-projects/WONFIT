97232eaecdad8afc4318515cd0f3b58cd318633d
Jaewonnnn <yjw970710@gmail.com>
2023-08-14 18:58:49 +0900

    init : backend 초기 프로젝트 셋팅

---
+++ backend/src/main/java/com/woori/wonfit/WonfitApplication.java
@@ -0,0 +1,13 @@
+package com.woori.wonfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonfitApplication.class, args);
+	}
+
+}
