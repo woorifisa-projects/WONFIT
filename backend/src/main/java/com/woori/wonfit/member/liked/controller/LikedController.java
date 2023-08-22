c3b670801c161b0fd105bb195e6527abb1d0b976
young <dbsduddls95@naver.com>
2023-08-22 15:59:11 +0900

    feat: 사용자 마이페이지 관심 상품 조회 API 기능 구현 중

--- backend/src/main/java/com/woori/wonfit/member/liked/controller/LikedController.java
+++ backend/src/main/java/com/woori/wonfit/member/liked/controller/LikedController.java
@@ -1,4 +1,24 @@
 package com.woori.wonfit.member.liked.controller;
 
+import com.woori.wonfit.member.liked.domain.Liked;
+import com.woori.wonfit.member.liked.service.LikedServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/mypage")
+@RequiredArgsConstructor
 public class LikedController {
+
+    private final LikedServiceImpl likedService;
+    @GetMapping("/{memberId}/liked")
+    public ResponseEntity<Liked> getLikedProductList(@PathVariable Long memberId) {
+        Liked likedList = likedService.getLikedProducts(memberId);
+        return ResponseEntity.ok(likedList);
+    }
 }
--- backend/src/main/java/com/woori/wonfit/product/productfund/domain/ProductFund.java
+++ backend/src/main/java/com/woori/wonfit/product/productfund/domain/ProductFund.java
@@ -1,5 +1,6 @@
 package com.woori.wonfit.product.productfund.domain;
 
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -8,4 +9,13 @@ public class ProductFund {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "fund_name")
+    private String fundName; // 펀드명
+
+    @Column(name = "fund_info")
+    private String fundInfo; // 상품 설명
+
+    @Column(name = "fund_risk_grade")
+    private String fundRiskGrade; // 투자위험등급
+
 }
