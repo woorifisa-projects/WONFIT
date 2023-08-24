1f9e6b7500a69b97d95454de5aa1d6109585078e
yoojaewon <yjw970710@gmail.com>
2023-08-24 11:58:03 +0900

    fix: 회원가입 버그 수정 (#72)

--- backend/src/main/java/com/woori/wonfit/member/member/dto/MemberRegisterRequest.java
+++ backend/src/main/java/com/woori/wonfit/member/member/dto/MemberRegisterRequest.java
@@ -15,6 +15,7 @@ public class MemberRegisterRequest {
     private String password; // 회원 패스워드
     private String email; // 회원 이메일
     private String name; // 회원 이름
+    private String registrationNumber;
     private boolean status; // 회원 상태
 
     public Member toEntity(String password) {
@@ -22,6 +23,7 @@ public class MemberRegisterRequest {
                 .loginId(loginId).password(password)
                 .email(email)
                 .name(name)
+                .registrationNumber(registrationNumber)
                 .status(status)
                 .build();
     }
