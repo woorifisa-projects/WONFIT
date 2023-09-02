316902e9a3124b49532dc0585ab90312a0aa7a95
yoojaewon <yjw970710@gmail.com>
2023-09-02 14:18:13 +0900

    feat: jenkins 빌드 오류 수정 및 허용 http 메소드 추가 (#130)
    * feat: 인가 기능 구현
    
    * feat: 인가 기능 구현
    
    * feat: jwt 유효시간 수정
    
    * feat: jwt 저장 추가
    
    * feat: access, refresh token 생성
    
    * fix: jenkins 빌드 오류 해결
    
    * chore: 허용 http 메소드 추가

--- backend/src/main/java/com/woori/wonfit/config/WebMvcConfig.java
+++ backend/src/main/java/com/woori/wonfit/config/WebMvcConfig.java
@@ -5,12 +5,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000") // 허용할 출처
-                .allowedMethods("GET", "POST","PUT") // 허용할 HTTP method
+                .allowedMethods("GET", "POST","PUT", "PATCH", "DELETE") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000);
     }
