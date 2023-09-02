e0063b7eddaeee713f1c384180f52f76c917c1d7
Jaewonnnn <yjw970710@gmail.com>
2023-09-02 14:15:09 +0900

    chore: 허용 http 메소드 추가

--- backend/src/main/java/com/woori/wonfit/config/WebMvcConfig.java
+++ backend/src/main/java/com/woori/wonfit/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000") // 허용할 출처
-                .allowedMethods("GET", "POST","PUT") // 허용할 HTTP method
+                .allowedMethods("GET", "POST","PUT", "PATCH", "DELETE") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000);
     }
