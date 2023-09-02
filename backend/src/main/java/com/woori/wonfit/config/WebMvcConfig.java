c4682bcc4283c0e71997fd02e52662fb0446b012
Jaewonnnn <yjw970710@gmail.com>
2023-09-02 14:07:15 +0900

    fix: jenkins 빌드 오류 해결

--- backend/src/main/java/com/woori/wonfit/config/WebMvcConfig.java
+++ backend/src/main/java/com/woori/wonfit/config/WebMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
