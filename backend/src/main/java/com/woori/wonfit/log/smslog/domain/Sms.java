c0b0ea8b8a29953f17cb1db6176a6bd592a7f70e
Jaewonnnn <yjw970710@gmail.com>
2023-09-03 22:32:24 +0900

    feat: SMS 메시지 발송 기록 DB 저장 완료

--- backend/src/main/java/com/woori/wonfit/log/smslog/domain/Sms.java
+++ backend/src/main/java/com/woori/wonfit/log/smslog/domain/Sms.java
@@ -1,5 +1,6 @@
 package com.woori.wonfit.log.smslog.domain;
 
+import com.woori.wonfit.log.smslog.dto.SmsRequest;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -15,11 +16,23 @@ public class Sms {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
     @Column(name = "sender")
-    private String sender;
+    private String to;
     @Column(name = "receiver")
-    private String receiver;
+    private String from;
     @Column(name = "content")
     private String content;
     @Column(name = "send_date")
     private String sendDate;
+    @Column(name = "status")
+    private boolean status;
+
+    public static Sms toEntity(SmsRequest request, String from, String date, boolean status){
+        return Sms.builder()
+                .to(request.getTo())
+                .from(from)
+                .content(request.getContent())
+                .sendDate(date)
+                .status(status)
+                .build();
+    }
 }
--- backend/src/main/java/com/woori/wonfit/log/smslog/service/SmsServiceImpl.java
+++ backend/src/main/java/com/woori/wonfit/log/smslog/service/SmsServiceImpl.java
@@ -2,20 +2,22 @@ package com.woori.wonfit.log.smslog.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.woori.wonfit.log.smslog.domain.MakeSignature;
+import com.woori.wonfit.log.smslog.domain.Sms;
 import com.woori.wonfit.log.smslog.dto.SmsRequest;
 import com.woori.wonfit.log.smslog.dto.SmsSendRequest;
 import com.woori.wonfit.log.smslog.dto.SmsSendResponse;
+import com.woori.wonfit.log.smslog.repository.SmsRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +27,7 @@ import java.util.List;
 public class SmsServiceImpl implements SmsService {
 
     private final MakeSignature makeSignature;
+    private final SmsRepository smsRepository;
 
     @Value("${access.key.id}")
     private String accessKey;
@@ -35,11 +38,14 @@ public class SmsServiceImpl implements SmsService {
     @Value("${sender.phone.number}")
     private String phoneNumber;
 
+    private LocalDateTime dateTime = LocalDateTime.now();
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    String date = dateTime.format(formatter).substring(0, 19);
+
     @Override
     public SmsSendResponse sendSms(SmsRequest smsRequest) throws Exception {
 
         Long time = System.currentTimeMillis();
-        log.info("phoneNumber = {}", phoneNumber);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -49,7 +55,6 @@ public class SmsServiceImpl implements SmsService {
 
         List<SmsRequest> messages = new ArrayList<>();
         messages.add(smsRequest);
-        log.info(messages.toString());
 
         SmsSendRequest request = SmsSendRequest.builder()
                 .type("SMS")
@@ -66,7 +71,10 @@ public class SmsServiceImpl implements SmsService {
 
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
-        SmsSendResponse response = restTemplate.postForObject(new URI("https://sens.apigw.ntruss.com/sms/v2/services/"+ serviceId +"/messages"), httpBody, SmsSendResponse.class);
+        SmsSendResponse response = restTemplate.postForObject(new URI("https://sens.apigw.ntruss.com/sms/v2/services/" + serviceId + "/messages"), httpBody, SmsSendResponse.class);
+
+        Sms sms = Sms.toEntity(smsRequest, phoneNumber, date, true);
+        smsRepository.save(sms);
 
         return response;
     }
