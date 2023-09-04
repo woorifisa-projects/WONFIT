25a82bf85901f90751b8b5b2a7a563a4674d9162
younnacho <nyc6709@gmail.com>
2023-09-04 11:49:56 +0900

    fix: SecurityConfig 파일 commit

--- backend/src/main/java/com/woori/wonfit/config/SecurityConfig.java
+++ backend/src/main/java/com/woori/wonfit/config/SecurityConfig.java
@@ -1,23 +1,23 @@
 package com.woori.wonfit.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+        import lombok.RequiredArgsConstructor;
+        import org.springframework.beans.factory.annotation.Value;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+        import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+        import org.springframework.security.config.http.SessionCreationPolicy;
+        import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+        import org.springframework.security.web.SecurityFilterChain;
+        import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig{
+public class SecurityConfig {
 
-    @Value("${jwt.token.secret}")
-    private String secretkey;
+    @Value("${jwt.token.access}")
+    private String accessKey;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -26,12 +26,12 @@ public class SecurityConfig{
                 .csrf().disable().headers().frameOptions().disable()
                 .and()
                 .authorizeRequests(auth -> auth.antMatchers("/member/login", "/member/register").permitAll()
-                        .antMatchers("/member/leave").hasAnyRole("USER").antMatchers("/manager/").hasAnyRole("ADMIN")
+                        .antMatchers("/member/**").hasAnyRole("USER")
                         .anyRequest().authenticated())
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .addFilterBefore(new JwtFilter(secretkey), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtFilter(accessKey), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
