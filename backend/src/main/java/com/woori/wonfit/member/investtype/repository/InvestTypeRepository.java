e6af2c00a3a4857a3a07358f75c2528f97b5794a
suwonhansibak <wndud562@gmail.com>
2023-09-04 18:40:52 +0900

    feat:투자유형 엔티티 및 api 작성

---
+++ backend/src/main/java/com/woori/wonfit/member/investtype/repository/InvestTypeRepository.java
@@ -0,0 +1,14 @@
+package com.woori.wonfit.member.investtype.repository;
+
+import com.woori.wonfit.member.investtype.domain.InvestType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InvestTypeRepository extends JpaRepository<InvestType,Long> {
+    List<InvestType> findAllByMemberId(Long memberId);
+
+    InvestType save(InvestType investType);
+}
