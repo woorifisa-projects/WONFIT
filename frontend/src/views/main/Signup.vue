8ba1e8b8e59c35b15cf195b0ba18c5e3abfaf326
younnacho <nyc6709@gmail.com>
2023-09-08 20:44:22 +0900

    feat: 회원가입 페이지 API 연결

--- frontend/src/views/main/Signup.vue
+++ frontend/src/views/main/Signup.vue
@@ -1,75 +1,135 @@
 <template>
-    <v-card
-      class="mx-auto"
-      max-width="500"
-      style="margin:50px"
-      title="Member Registration"
-    
-    >
+  <v-card class="mx-auto" max-width="500" style="margin: 50px" title="Member Registration">
+    <v-form @submit.prevent="signup">
       <v-container>
-     
         <v-text-field
           v-model="name"
           color="primary"
-          label="Name"
-          placeholder="Enter your name"
+          label="이름"
+          variant="underlined"
+        ></v-text-field>
+        <v-text-field
+          v-model="registrationNumber"
+          color="primary"
+          label="주민등록번호"
+          variant="underlined"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="bankAccountNumber"
+          color="primary"
+          label="계좌번호"
           variant="underlined"
         ></v-text-field>
 
         <v-text-field
           v-model="id"
           color="primary"
-          label="ID"
-          placeholder="Enter your id"
+          label="아이디"
+          hint="5자 이상 15자 이하로 입력해 주세요."
+          maxLength="15"
           variant="underlined"
         ></v-text-field>
 
         <v-text-field
           v-model="password"
           color="primary"
-          label="Password"
-          placeholder="Enter your password"
+          label="비밀번호"
+          hint="8자 이상 20자 이하로 입력해 주세요."
+          maxLength="20"
           variant="underlined"
         ></v-text-field>
-  
-  
+
         <v-text-field
           v-model="email"
           color="primary"
-          label="Email"
-          placeholder="Enter your email"
+          label="이메일"
           variant="underlined"
         ></v-text-field>
-  
-        
+
+        <v-text-field
+          v-model="phoneNumber"
+          color="primary"
+          label="전화번호"
+          variant="underlined"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="address"
+          color="primary"
+          label="주소"
+          variant="underlined"
+        ></v-text-field>
+
         <v-checkbox
           v-model="terms"
           color="secondary"
           label="I agree to WooriWONFIT terms and conditions"
         ></v-checkbox>
       </v-container>
-  
+
       <v-divider></v-divider>
-  
+
       <v-card-actions>
         <v-spacer></v-spacer>
-  
-        <v-btn color="blue">
+
+        <v-btn type="submit" color="blue">
           Complete Registration
-  
+
           <v-icon icon="mdi-chevron-right" end></v-icon>
         </v-btn>
       </v-card-actions>
-    </v-card>
-  </template>
-  <script>
-    export default {
-      data: () => ({
-        name: null,
-        id: null,
-        password: null,
-        email: null,
-        terms: false,
-      }),
+    </v-form>
+  </v-card>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+import { postApi } from "@/api/modules";
+import { ref } from "vue";
+
+const name = ref();
+const registrationNumber = ref();
+const bankAccountNumber = ref();
+const id = ref();
+const password = ref();
+const address = ref();
+const email = ref();
+const phoneNumber = ref();
+const terms = ref();
+
+const signup = async () => {
+  try {
+    const requestBody = {
+      name: name.value,
+      registrationNumber: registrationNumber.value,
+      bankAccountNumber: bankAccountNumber.value,
+      loginId: id.value,
+      password: password.value,
+      status: true,
+      address: address.value,
+      email: email.value,
+      phoneNumber: phoneNumber.value,
+      terms: terms.value,
+    };
+
+    const response = await postApi({
+      url: `/wonfit/register`,
+      data: requestBody,
+    });
+
+    if (response.status == 200) {
+      // 쿠키 저장 메소드
+      router.push({ name: "MainPage" });
+
+      // 요청이 성공하면 적절한 처리를 수행합니다.
+      console.log("데이터 업데이트 성공:", response);
     }
-  </script>
+  } catch (error) {
+    // 요청이 실패하면 오류 처리를 수행합니다.
+    console.error("데이터 업데이트 오류:", error);
+  }
+};
+</script>
