e5315c59bbfe7aae821a4e438064d4e7e6987f5b
yoojaewon <yjw970710@gmail.com>
2023-09-08 20:50:14 +0900

    feat: sms 발송 내역 조회 기능 구현 (#175)

--- backend/src/main/java/com/woori/wonfit/log/smslog/service/SmsService.java
+++ backend/src/main/java/com/woori/wonfit/log/smslog/service/SmsService.java
@@ -1,8 +1,12 @@
 package com.woori.wonfit.log.smslog.service;
 
+import com.woori.wonfit.log.smslog.domain.Sms;
 import com.woori.wonfit.log.smslog.dto.SmsRequest;
 import com.woori.wonfit.log.smslog.dto.SmsSendResponse;
 
+import java.util.List;
+
 public interface SmsService {
     SmsSendResponse sendSms(SmsRequest request) throws Exception;
+    List<Sms> getAllSms();
 }
