f64f67b3b391b1f4faa4bd802adf1d554e4a8201
younnacho <nyc6709@gmail.com>
2023-09-11 17:39:35 +0900

    fix: MyPage 수정

--- backend/src/main/java/com/woori/wonfit/member/member/repository/MemberRepository.java
+++ backend/src/main/java/com/woori/wonfit/member/member/repository/MemberRepository.java
@@ -1,6 +1,5 @@
 package com.woori.wonfit.member.member.repository;
 
-
 import com.woori.wonfit.member.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
--- frontend/src/views/member/MyPage.vue
+++ frontend/src/views/member/MyPage.vue
@@ -1,202 +1,206 @@
 <template>
-  <div>
-    <!-- 배너 영역 -->
-    <div class="banner-container d-flex justify-center">
-      <!-- 배너 이미지 추가 -->
-      <h1 style="margin: 120px 210px">
-        우리WONFIT에서<br />
-        나에게 딱 맞게
-      </h1>
-      <img src="@/assets/puzzler.png" class="banner-image" />
-    </div>
+  <v-app id="inspire" style="margin-top: 20px">
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="3">
+            <v-sheet rounded="lg">
+              <v-list rounded="lg">
+                <v-list-item class="logo-text" @click="navigateToMyPage">내 정보 보기</v-list-item>
+                <v-list-item class="logo-text" @click="navigateToMySubscribeProduct"
+                  >내 가입상품 확인하기</v-list-item
+                >
+                <v-list-item class="logo-text" @click="navigateToMyLikedProduct"
+                  >내 관심상품 확인하기</v-list-item
+                >
+                <v-list-item class="logo-text" @click="navigateToRecommend"
+                  >나의 투자성향 보기</v-list-item
+                >
+                <v-divider class="my-2"></v-divider>
+                <v-list-item
+                  class="logo-text"
+                  color="grey-lighten-4"
+                  link
+                  @click="navigateToWithdraw"
+                >
+                  회원 탈퇴
+                </v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
 
-    <div class="banner-content">
-      <h2>목적에 맞는 금융상품을</h2>
-      <p>성향분석 검사를 통해 편리하게 추천 받으세요</p>
-    </div>
+          <v-col>
+            <v-sheet class="logo-text" style="padding: 40px" min-height="70vh" rounded="lg">
+              <div>
+                <v-form @submit.prevent="saveChanges">
+                  <h4>이름</h4>
+                  <v-text-field
+                    disabled
+                    :value="memberData.name"
+                    variant="underlined"
+                  ></v-text-field
+                  ><br />
+                  <h4>주민등록번호</h4>
+                  <v-text-field
+                    disabled
+                    :value="memberData.registrationNumber"
+                    variant="underlined"
+                  ></v-text-field
+                  ><br />
 
-    <v-row class="d-flex justify-center">
-      <div class="image-with-text-container clickable-title" @click="navigateToDepositTestPage">
-        <img src="@/assets/piece.png" class="puzzle-image image" />
-        <div class="text mt-n2" style="font-size: 18px">
-          예금<br />
-          퍼즐찾기
-        </div>
-      </div>
+                  <h4>계좌번호</h4>
+                  <v-text-field
+                    disabled
+                    :value="memberData.bankAccountNumber"
+                    variant="underlined"
+                  ></v-text-field
+                  ><br />
 
-      <div class="image-with-text-container clickable-title" @click="navigateToSavingsTestPage">
-        <img src="@/assets/piece.png" class="puzzle-image image" />
-        <div class="text mt-n2" style="font-size: 18px">
-          적금<br />
-          퍼즐찾기
-        </div>
-      </div>
+                  <h4>아이디</h4>
+                  <v-text-field
+                    disabled
+                    :value="memberData.loginId"
+                    variant="underlined"
+                  ></v-text-field
+                  ><br />
 
-      <div class="image-with-text-container clickable-title" @click="navigateToFundTestPage">
-        <img src="@/assets/piece.png" class="puzzle-image image" />
-        <div class="text mt-n2" style="font-size: 18px">
-          펀드<br />
-          퍼즐찾기
-        </div>
-      </div>
+                  <h4>비밀번호</h4>
 
-      <div class="image-with-text-container clickable-title" @click="navigateToLoanTestPage">
-        <img src="@/assets/piece.png" class="puzzle-image image" />
-        <div class="text mt-n2" style="font-size: 18px">
-          대출<br />
-          퍼즐찾기
-        </div>
-      </div>
-    </v-row>
+                  <v-text-field
+                    :disabled="!isEditMode"
+                    v-model="memberData.password"
+                    variant="underlined"
+                  ></v-text-field
+                  ><br />
 
-    <!-- 배너 영역 -->
-    <div class="banner-container2 d-flex justify-center">
-      <!-- 배너 이미지 추가 -->
-      <h1 style="margin: 100px 200px">
-        모든 금융내역을<br />
-        한 곳에서 관리하세요 <br />
-        <v-btn
-            color="#2965ED"
-            height="42px"
-            rounded="lg"
-            style="margin-top: 30px; font-weight: bold; color: white"
-            @click="navigateToIncomeAnalysisPage"
-        >마이데이터 보러가기</v-btn
-        >
-      </h1>
-      <img src="@/assets/receipt.png" class="banner-image" />
-    </div>
-  </div>
+                  <h4>이메일</h4>
+                  <v-text-field
+                    :disabled="!isEditMode"
+                    v-model="memberData.email"
+                    variant="underlined"
+                  ></v-text-field
+                  ><br />
+
+                  <h4>전화번호</h4>
+                  <v-text-field
+                    :disabled="!isEditMode"
+                    v-model="memberData.phoneNumber"
+                    variant="underlined"
+                  ></v-text-field
+                  ><br />
+
+                  <h4>주소</h4>
+
+                  <v-text-field
+                    :disabled="!isEditMode"
+                    v-model="memberData.address"
+                    variant="underlined"
+                  ></v-text-field
+                  ><br />
+                  <div class="d-flex justify-center">
+                    <v-btn text class="mx-2" @click="toggleEditMode">수정하기</v-btn>
+                    <v-btn type="submit" text>저장하기</v-btn>
+                  </div>
+                </v-form>
+              </div>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script setup>
 import { useRouter } from "vue-router";
+import { ref, onBeforeMount } from "vue";
+import { getApi } from "@/api/modules";
+// import { patchApi } from "@/api/modules";
+
+import axios from "axios";
+
+//axios.defaults.withCredentials = true;
+
+const isEditMode = ref(false);
+const memberData = ref({
+  name: "",
+  registrationNumber: "",
+  loginId: "",
+  password: "",
+  email: "",
+  phoneNumber: "",
+  bankAccountNumber: "",
+  address: "",
+});
+
+const toggleEditMode = () => {
+  isEditMode.value = !isEditMode.value;
+};
+
+const saveChanges = async () => {
+  try {
+    const updatedData = {
+      name: memberData.value.name,
+      registrationNumber: memberData.value.registrationNumber,
+      loginId: memberData.value.loginId,
+      password: memberData.value.password,
+      email: memberData.value.email,
+      phoneNumber: memberData.value.phoneNumber,
+      bankAccountNumber: memberData.value.bankAccountNumber,
+      address: memberData.value.address,
+    };
+
+    const response = await axios.patch("http://localhost:8080/member/detail", updatedData, {
+      withCredentials: true,
+    });
+
+    console.log("데이터 업데이트 성공:", response);
+  } catch (error) {
+    console.error("데이터 업데이트 오류:", error);
+  }
+};
+
+onBeforeMount(async () => {
+  const data = await getApi({
+    url: `/member/detail`,
+  });
+  memberData.value = data;
+});
+
 const router = useRouter();
 
-const navigateToIncomeAnalysisPage = () => {
-  router.push({ name: "Income" });
+const navigateToMyPage = () => {
+  router.push({ name: "MyPage" });
 };
 
-const navigateToFundTestPage = () => {
-  router.push({ name: "FundTest" });
+const navigateToMySubscribeProduct = () => {
+  router.push({ name: "MySubscribeProduct" });
 };
 
-const navigateToLoanTestPage = () => {
-  router.push({ name: "LoanTest" });
+const navigateToMyLikedProduct = () => {
+  router.push({ name: "MyLikedProduct" });
 };
 
-const navigateToDepositTestPage = () => {
-  router.push({ name: "DepositTest" });
+const navigateToRecommend = () => {
+  router.push({ name: "Recommend" });
 };
 
-const navigateToSavingsTestPage = () => {
-  router.push({ name: "SavingsTest" });
+const navigateToWithdraw = () => {
+  router.push({ name: "Withdraw" });
 };
 </script>
 
-<style lang="scss" scoped>
-.banner-container {
-  background-color: #f6f7ff; /* 배경색 설정 */
-  padding: 20px; /* 내부 여백 추가 */
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-top: 10px;
+<style scoped>
+@font-face {
+  font-family: "WooridaumB";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2205@1.0/WooridaumB.woff2")
+    format("woff2");
+  font-weight: 300;
+  font-style: normal;
 }
 
-.banner-container2 {
-  background-color: #f6f7ff; /* 배경색 설정 */
-  padding: 20px; /* 내부 여백 추가 */
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  margin-bottom: 50px;
-}
-
-/* 배너 이미지 스타일 */
-.banner-image {
-  width: 210px; /* 이미지를 부모 요소의 너비에 맞춤 */
-  max-height: 300px; /* 이미지의 최대 높이 설정 */
-  margin-right: 250px;
-  object-fit: cover; /* 이미지를 부모 요소에 맞추고 가로/세로 비율 유지 */
-}
-
-.puzzle-image {
-  display: flex;
-  align-items: center;
-  width: 200px; /* 이미지를 부모 요소의 너비에 맞춤 */
-  max-height: 300px; /* 이미지의 최대 높이 설정 */
-  margin-right: 20px;
-}
-
-/* 배너 내용 스타일 */
-.banner-content {
-  margin-top: 60px;
-  margin-bottom: 70px;
-  text-align: center;
-}
-
-.banner-content h2 {
-  font-size: 24px;
-  color: #333; /* 텍스트 색상 설정 */
-}
-
-.banner-content p {
-  font-size: 16px;
-  color: #666; /* 텍스트 색상 설정 */
-}
-
-/* 각 span 요소를 중앙 정렬하고 간격을 조정합니다 */
-.banner-content span {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 10px; /* 간격 조정 */
-}
-.puzzle-text {
-  position: absolute;
-  top: 50%; /* 텍스트를 이미지 상단 중앙에 배치합니다 */
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-size: 16px;
-  color: #fff; /* 텍스트 색상 설정 */
-  text-align: center;
-  background-color: rgba(0, 0, 0, 0.7); /* 텍스트 배경색과 투명도 설정 */
-  padding: 5px 10px;
-}
-
-/* 이미지와 텍스트 컨테이너 스타일 */
-.image-with-text-container {
-  position: relative;
-  display: inline-block; /* 컨테이너가 이미지와 텍스트의 크기에 맞게 조절되도록 설정 */
-  margin-bottom: 100px;
-}
-
-/* 이미지 스타일 */
-.image {
-  max-width: 100%; /* 이미지를 컨테이너의 너비에 맞춤 */
-  height: auto; /* 이미지 높이를 자동으로 조절하여 비율 유지 */
-}
-
-/* 텍스트 스타일 */
-.text {
-  position: absolute;
-  top: 81px; /* 텍스트를 이미지 상단에 배치하려면 필요한 위치 조절 */
-  left: 55px; /* 텍스트를 이미지 왼쪽에 배치하려면 필요한 위치 조절 */
+.logo-text {
   font-size: 15px;
-  color: white;
-  background-color: #00acea; /* 텍스트 배경색과 투명도 설정 */
-  padding: 5px 10px;
-}
-
-.clickable-title:hover {
-  text-shadow: 0px 1px 2px rgba(0, 0, 0, 0.2);
-  cursor: pointer;
-}
-
-.image-with-text-container:hover {
-  transform: scale(1.05);
-  transition: transform 0.5s;
+  font-family: "WooridaumB", sans-serif;
 }
 </style>
