ebe1b62c3098125c31c5867e0105112a73c83198
yoojaewon <yjw970710@gmail.com>
2023-09-11 20:09:27 +0900

    feat: 알림톡 페이지 구현 (#197)

--- frontend/src/views/manager/SendTalk.vue
+++ frontend/src/views/manager/SendTalk.vue
@@ -6,66 +6,71 @@
           <v-col cols="3">
             <v-sheet rounded="lg">
               <v-list rounded="lg">
-                <v-list-item class="logo-text" @click="navigateToManagerPage"
-                  >사용자 관리</v-list-item
-                >
-                <v-list-item class="logo-text" @click="navigateToManageProduct"
-                  >상품 관리</v-list-item
-                >
+                <v-list-item class="logo-text" @click="navigateToManagerPage">사용자 관리</v-list-item>
+                <v-list-item class="logo-text" @click="navigateToManageProduct">상품 관리</v-list-item>
                 <v-list-item class="logo-text" @click="navigateToManageLog"> 로그 관리</v-list-item>
-                <v-list-item class="logo-text" @click="navigateToSendTalk"
-                  >알림톡 보내기</v-list-item
-                >
+                <v-list-item class="logo-text" @click="navigateToSendTalk">SMS</v-list-item>
               </v-list>
             </v-sheet>
           </v-col>
-
           <v-col>
             <v-sheet class="logo-text" style="padding: 10px" min-height="70vh" rounded="lg">
               <div>
-                <v-table fixed-header height="300px">
+                <v-table fixed-header height="700px">
                   <thead>
                     <tr>
                       <th></th>
                       <th class="text-left">번호</th>
-                      <th class="text-left">성함</th>
-                      <th class="text-left">아이디</th>
-                      <th class="text-left">회원상태</th>
-                      <th class="text-left">가입일자</th>
+                      <th class="text-left">일시</th>
+                      <th class="text-left">발신자 번호</th>
+                      <th class="text-left">수신자 번호</th>
+                      <th class="text-left">메시지 내용</th>
+                      <th class="text-left">상태</th>
                     </tr>
                   </thead>
                   <template v-slot:column.name="{ column }">
                     {{ column.title.toUpperCase() }}
                   </template>
                   <tbody>
-                    <tr v-for="item in users" :key="item.idx">
+                    <tr v-for="item in reversedSmsData" :key="item.idx">
                       <v-checkbox style="margin-top: 1.3rem"></v-checkbox>
-                      <td>{{ item.idx }}</td>
-                      <td>{{ item.name }}</td>
                       <td>{{ item.id }}</td>
-                      <td>{{ item.status }}</td>
-                      <td>{{ item.rgDate }}</td>
+                      <td>{{ item.sendDate }}</td>
+                      <td>{{ item.from }}</td>
+                      <td>{{ item.to }}</td>
+                      <td>{{ item.content }}</td>
+                      <td>
+                        <span v-if="item.status">O</span>
+                        <span v-else>X</span>
+                      </td>
                     </tr>
                   </tbody>
                 </v-table>
               </div>
-
-              <div class="d-flex justify-center" style="padding-bottom: 30px">
-                <form @submit.prevent="sendMessage">
-                  <input type="text" v-model="senderPhoneNumber" placeholder="발신자 전화번호" />
-                  <input type="text" v-model="receiverPhoneNumber" placeholder="수신자 전화번호" />
-                  <br /><br />
-                  <!-- <input type="text" v-model="messageContent" placeholder="메시지 내용을 입력해주세요."><br><br> -->
-                  <!-- <button type="submit">메시지 전송</button> -->
-
-                  <v-text-field
-                    v-model="messageContent"
-                    placeholder="메시지 내용을 입력해주세요."
-                  ></v-text-field>
-                  <v-btn color="blue" style="margin-left: 100px"> 메시지 전송 </v-btn>
-                </form>
-              </div>
             </v-sheet>
+            <v-col class="d-flex align-center">
+              <div class="d-flex justify-end align-center" style="width: 100%; height: 100%;">
+                <div class="d-flex justify-end"> <!-- 이 부분 추가 -->
+                  <v-btn color="blue" style="margin-left: 100px" @click="showModal = true">메시지 전송</v-btn>
+                </div>
+              </div>
+            </v-col>
+            <v-dialog v-model="showModal" max-width="500px">
+              <v-card>
+                <v-card-title>메시지 전송</v-card-title>
+                <v-card-text>
+                  <form @submit.prevent="sendMessage">
+                    <v-select :items="['010-3203-7238']" label="발신자 전화번호" required></v-select>
+                    <v-text-field v-model="receiverPhoneNumberModal" label="수신자 전화번호"></v-text-field>
+                    <v-textarea v-model="messageContentModal" label="메시지 내용" maxlength="90"></v-textarea>
+                    <div class="text-right">
+                      <v-btn color="blue" text @click.native="showModal = false">취소</v-btn>
+                      <v-btn color="blue" text type="submit">전송</v-btn>
+                    </div>
+                  </form>
+                </v-card-text>
+              </v-card>
+            </v-dialog>
           </v-col>
         </v-row>
       </v-container>
@@ -75,71 +80,50 @@
 
 <script setup>
 import { ref } from "vue";
-import CryptoJS from "crypto-js";
+import { onMounted } from "vue";
+import { computed } from 'vue';
 
-const senderPhoneNumber = ref("");
-const receiverPhoneNumber = ref("");
-const messageContent = ref("");
+import axios from 'axios';
 
-function sendMessage() {
-  var user_phone_number = receiverPhoneNumber.value;
-  var resultCode = 404;
+const showModal = ref(false);
 
-  const date = Date.now().toString();
-  const uri = "ncp:sms:kr:314756095679:wonfit-sms-service";
-  const secretKey = "PvqIcNh9p4lJpr6IGM2wXDUAxOpHVb7plnCAoids";
-  const accessKey = "nzsn8K2TrvZIx6Nl9C5I";
-  const method = "POST";
-  const space = " ";
-  const newLine = "\n";
-  const url2 = `/sms/v2/services/${uri}/messages`;
+const smsData = ref([]);
+const reversedSmsData = computed(() => [...smsData.value].reverse());
 
-  const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
+onMounted(fetchSmsData);
 
-  hmac.update(method);
-  hmac.update(space);
-  hmac.update(url2);
-  hmac.update(newLine);
-  hmac.update(date);
-  hmac.update(newLine);
-  hmac.update(accessKey);
+async function fetchSmsData() {
+  try {
+    const response = await axios.get("http://localhost:8080/manager/sms", { withCredentials: true });
+    smsData.value = response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-  const hashBytesArray = hmac.finalize();
-  const signature = hashBytesArray.toString(CryptoJS.enc.Base64);
 
-  const url = `https://sens.apigw.ntruss.com/sms/v2/services/${uri}/messages`;
+const receiverPhoneNumberModal = ref("");
+const messageContentModal = ref("");
+
+async function sendMessage() {
   const body = {
-    type: "SMS",
-    countryCode: "82",
-    from: senderPhoneNumber.value,
-    content: messageContent.value,
-    messages: [
-      {
-        to: user_phone_number,
-      },
-    ],
+    to: receiverPhoneNumberModal.value,
+    content: messageContentModal.value,
   };
 
-  fetch(url, {
-    //mode: 'no-cors',
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      "x-ncp-apigw-timestamp": date,
-      "x-ncp-iam-access-key": accessKey,
-      "x-ncp-apigw-signature-v2": signature,
-    },
-    body: JSON.stringify(body),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      resultCode = 200;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  try {
+    const response = await axios.post("http://localhost:8080/manager/sms", body, { withCredentials: true });
+    console.log(response.data);
+
+    showModal.value = false;
+
+    fetchSmsData();
+
+  } catch (error) {
+    console.error(error);
+  }
 }
+
 
 import { useRouter } from "vue-router";
 
@@ -165,8 +149,7 @@ const navigateToManageLog = () => {
 <style scoped>
 @font-face {
   font-family: "WooridaumB";
-  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2205@1.0/WooridaumB.woff2")
-    format("woff2");
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2205@1.0/WooridaumB.woff2") format("woff2");
   font-weight: 300;
   font-style: normal;
 }
@@ -174,5 +157,16 @@ const navigateToManageLog = () => {
 .logo-text {
   font-size: 15px;
   font-family: "WooridaumB", sans-serif;
+}
+
+.d-flex.align-center {
+  display: flex;
+  align-items: center;
+  justify-content: right;
+  min-height: 5vh;
+}
+
+.text-right {
+  text-align: right;
 }
 </style>
