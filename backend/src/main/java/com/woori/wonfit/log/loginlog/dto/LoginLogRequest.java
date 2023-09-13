b2998811b08e46b1ec92003c5caf77ec1f7c9290
Jaewonnnn <yjw970710@gmail.com>
2023-09-13 18:09:13 +0900

    feat: 로그인 기록 조회 기능 구현

---
+++ backend/src/main/java/com/woori/wonfit/log/loginlog/dto/LoginLogRequest.java
@@ -0,0 +1,8 @@
+package com.woori.wonfit.log.loginlog.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginLogRequest {
+    private String loginId;
+}
---
+++ backend/src/main/java/com/woori/wonfit/log/loginlog/service/LoginLogServiceImpl.java
@@ -0,0 +1,31 @@
+package com.woori.wonfit.log.loginlog.service;
+
+import com.woori.wonfit.log.loginlog.domain.LoginLog;
+import com.woori.wonfit.log.loginlog.dto.LoginLogRequest;
+import com.woori.wonfit.log.loginlog.repository.LoginLogRepository;
+import com.woori.wonfit.member.member.domain.Member;
+import com.woori.wonfit.member.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LoginLogServiceImpl implements LoginLogService {
+    private final LoginLogRepository loginLogRepository;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public List<LoginLog> getAllLoginLog(String id) {
+        List<LoginLog> loginLogs = loginLogRepository.findAllByMemberId(Long.parseLong(id));
+        return loginLogs;
+    }
+
+    @Override
+    public List<LoginLog> getAllLoginLog(LoginLogRequest request) {
+        Member member = memberRepository.findByLoginId(request.getLoginId()).get();
+        List<LoginLog> loginLogs = getAllLoginLog(member.getId().toString());
+        return loginLogs;
+    }
+}
