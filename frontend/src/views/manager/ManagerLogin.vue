63a8a84cb77d82bb3749960971f9d1fc4c7d6849
Jaewonnnn <yjw970710@gmail.com>
2023-09-14 11:14:11 +0900

    feat: 매니저 로그인 페이지 구현

---
+++ frontend/src/views/manager/ManagerLogin.vue
@@ -0,0 +1,74 @@
+<template>
+  <div style="margin-top: 50px">
+    <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
+      <div class="text-subtitle-1 text-medium-emphasis">ID</div>
+      <v-form @submit.prevent="login">
+        <v-text-field density="compact" placeholder="Enter your id" prepend-inner-icon="mdi-email-outline"
+          variant="outlined" v-model="loginId"></v-text-field>
+
+        <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+          Password
+
+          <a class="text-caption text-decoration-none text-blue" href="#" rel="noopener noreferrer" target="_blank">
+            Forgot login password?
+          </a>
+        </div>
+
+        <v-text-field :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'" :type="visible ? 'text' : 'password'"
+          density="compact" placeholder="Enter your password" prepend-inner-icon="mdi-lock-outline" v-model="password"
+          variant="outlined" @click:append-inner="visible = !visible"></v-text-field>
+
+        <v-btn type="submit" block class="mb-8" color="blue" size="large" variant="tonal">
+          Log In
+        </v-btn>
+
+        <v-card-text class="text-center">
+          <a class="text-blue text-decoration-none" href="#" rel="noopener noreferrer" @click="navigateToSignup">
+            Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+          </a>
+        </v-card-text>
+      </v-form>
+    </v-card>
+  </div>
+</template>
+
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { useAuthStore } from "@/store/useAuthStore";
+
+const auth = useAuthStore();
+
+const router = useRouter();
+
+const navigateToSignup = () => {
+  router.push({ name: "Signup" });
+};
+
+const visible = ref(false);
+const loginId = ref("");
+const password = ref("");
+
+const login = async () => {
+  try {
+    axios.defaults.withCredentials = true;
+
+    const requestBody = {
+      loginId: loginId.value,
+      password: password.value,
+    };
+
+    const response = await axios.post("https://back.wonfit.site/wonfit/login", requestBody, {
+      withCredentials: true,
+    });
+    if (response.status == 200) {
+      // 쿠키 저장 메소드
+      auth.setIsLogin(true);
+      router.push({ name: "MainPage" });
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+};
+</script>
