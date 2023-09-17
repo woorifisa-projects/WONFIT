bb67e10598a754becbce7607d783dd5ebd84a4db
young <purple11_11@naver.com>
2023-09-17 19:04:16 +0900

    feat: 스크롤 위치 유지

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -250,6 +250,9 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior() {
+    return { top: 0 };
+  },
 });
 
 router.beforeEach(async (to) => {
