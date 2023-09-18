5b665bd21d9dec7c6fa94da823a26929d32edc4f
young <purple11_11@naver.com>
2023-09-18 09:55:22 +0900

    HOTFIX: 관리자 로그인 store 설정

--- frontend/src/store/modules/useAuthStore.js
+++ frontend/src/store/modules/useAuthStore.js
@@ -4,6 +4,7 @@ import { ref } from "vue";
 export const useAuthStore = defineStore("auth", () => {
   // 로그인 상태
   const isLogin = ref(false);
+  const isAdmin = ref(false);
 
   const login = () => {
     isLogin.value = true;
@@ -16,8 +17,8 @@ export const useAuthStore = defineStore("auth", () => {
   };
 
   const initializeAuth = () => {
-    const sotredIsLogin = localStorage.getItem("isLogin");
-    if (sotredIsLogin) {
+    const storedIsLogin = localStorage.getItem("isLogin");
+    if (storedIsLogin) {
       isLogin.value = true;
     } else {
       isLogin.value = false;
@@ -29,5 +30,9 @@ export const useAuthStore = defineStore("auth", () => {
     isLogin.value = value;
   };
 
-  return { isLogin, setIsLogin, login, logout, initializeAuth };
+  const setIsAdmin = (value) => {
+    isAdmin.value = value;
+  };
+
+  return { isLogin, isAdmin, setIsLogin, setIsAdmin, login, logout, initializeAuth };
 });
